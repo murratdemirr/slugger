ed1e86064854f4d6fae7e2f1dadd8cc7632439b9
MURAT DEMIR <murat.demir@etstur.com>
2018-08-08 14:17:35 +0300

    first commit

---
+++ vacuum/src/main/java/com/demir/vacuum/VacuumApplication.java
@@ -0,0 +1,12 @@
+package com.demir.vacuum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacuumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacuumApplication.class, args);
+    }
+}
