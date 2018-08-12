53b917397c1a9cddd3802b7f622a5df6bce3795e
muratdemir <murrat.demirr@gmail.com>
2018-08-12 12:29:15 +0300

    dataset xml pojo

---
+++ edge/src/main/java/com/demir/edge/dataset/entity/DatasetType.java
@@ -0,0 +1,90 @@
+
+package com.demir.edge.dataset.entity;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for datasetType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="datasetType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="emails" type="{}emailsType"/>
+ *         &lt;element name="resources" type="{}resourcesType"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "datasetType", propOrder = {
+    "emails",
+    "resources"
+})
+public class DatasetType {
+
+    @XmlElement(required = true)
+    protected EmailsType emails;
+    @XmlElement(required = true)
+    protected ResourcesType resources;
+
+    /**
+     * Gets the value of the emails property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link EmailsType }
+     *     
+     */
+    public EmailsType getEmails() {
+        return emails;
+    }
+
+    /**
+     * Sets the value of the emails property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link EmailsType }
+     *     
+     */
+    public void setEmails(EmailsType value) {
+        this.emails = value;
+    }
+
+    /**
+     * Gets the value of the resources property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link ResourcesType }
+     *     
+     */
+    public ResourcesType getResources() {
+        return resources;
+    }
+
+    /**
+     * Sets the value of the resources property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link ResourcesType }
+     *     
+     */
+    public void setResources(ResourcesType value) {
+        this.resources = value;
+    }
+
+}
---
+++ edge/src/main/java/com/demir/edge/dataset/entity/EmailsType.java
@@ -0,0 +1,72 @@
+
+package com.demir.edge.dataset.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for emailsType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="emailsType">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="email" maxOccurs="unbounded" minOccurs="0">
+ *           &lt;simpleType>
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *             &lt;/restriction>
+ *           &lt;/simpleType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "emailsType", propOrder = {
+    "email"
+})
+public class EmailsType {
+
+    protected List<String> email;
+
+    /**
+     * Gets the value of the email property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the email property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getEmail().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     * 
+     * 
+     */
+    public List<String> getEmail() {
+        if (email == null) {
+            email = new ArrayList<String>();
+        }
+        return this.email;
+    }
+
+}
